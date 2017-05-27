226788ef94dd537675ea0c74cc8ee14e39671fe5
FrancescoRigoni <francesco.rigoni@gmail.com>
2017-05-27 18:02:49 +0200

    Refactored ORA OpCodes

--- cpu/OpCode_ORA.cpp
+++ cpu/OpCode_ORA.cpp
@@ -2,7 +2,7 @@
 
 #include <cmath>
 
-#define LOG_TAG "Cpu::executeAND"
+#define LOG_TAG "Cpu::executeORA"
 
 /**
  * This file contains implementations for all ORA OpCodes.
@@ -25,181 +25,120 @@ void Cpu::executeORA16Bit(OpCode &opCode) {
 }
 
 void Cpu::executeORA(OpCode &opCode) {
+    if (accumulatorIs8BitWide()) {
+        executeORA8Bit(opCode);
+    } else {
+        executeORA16Bit(opCode);
+        addToCycles(1);
+    }
+
     switch (opCode.getCode()) {
-        case (0x01):                // ORA DP Indexed Indirect, X
+        case (0x09):                // ORA Immediate
         {
-            int opCycles = Binary::lower8BitsOf(mD) != 0 ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-            } else {
-                executeORA16Bit(opCode);
-                opCycles++;
+            if (accumulatorIs16BitWide()) {
+                addToProgramAddress(1);
             }
-            addToProgramAddressAndCycles(2, 6 + opCycles);
+            addToProgramAddressAndCycles(2, 2);
             break;
         }
-        case (0x03):                // ORA Stack Relative
+        case (0x0D):                // ORA Absolute
         {
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-                addToProgramAddressAndCycles(2, 4);
-            } else {
-                executeORA16Bit(opCode);
-                addToProgramAddressAndCycles(2, 5);
-            }
+            addToProgramAddressAndCycles(3, 4);
+            break;
+        }
+        case (0x0F):                // ORA Absolute Long
+        {
+            addToProgramAddressAndCycles(4, 5);
             break;
         }
         case(0x05):                 // ORA Direct Page
         {
-            int opCycles = Binary::lower8BitsOf(mD) != 0 ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-            } else {
-                executeORA16Bit(opCode);
-                opCycles++;
+            if (Binary::lower8BitsOf(mD) != 0) {
+                addToCycles(1);
             }
-            addToProgramAddressAndCycles(2, 3+opCycles);
+            addToProgramAddressAndCycles(2, 3);
             break;
         }
-        case(0x07):                 // ORA Direct page indirect long
+        case(0x12):                 // ORA Direct Page Indirect
         {
-            int opCycles = Binary::lower8BitsOf(mD) != 0 ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-            } else {
-                executeORA16Bit(opCode);
-                opCycles++;
+            if (Binary::lower8BitsOf(mD) != 0) {
+                addToCycles(1);
             }
-            addToProgramAddressAndCycles(2, 6+opCycles);
+            addToProgramAddressAndCycles(2, 5);
             break;
         }
-        case (0x09):                // ORA Immediate
+        case(0x07):                 // ORA Direct Page Indirect Long
         {
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-                addToProgramAddressAndCycles(2,2);
-            } else {
-                executeORA16Bit(opCode);
-                addToProgramAddressAndCycles(3,3);
+            if (Binary::lower8BitsOf(mD) != 0) {
+                addToCycles(1);
             }
-            break;
-        }
-        case (0x0D):                // ORA absolute
-        {
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-                addToProgramAddressAndCycles(3,4);
-            } else {
-                executeORA16Bit(opCode);
-                addToProgramAddressAndCycles(3,5);
-            }
-            break;
-        }
-        case (0x0F):                // ORA absolute long
-        {
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-                addToProgramAddressAndCycles(4,5);
-            } else {
-                executeORA16Bit(opCode);
-                addToProgramAddressAndCycles(4,6);
-            }
-            break;
-        }
-        case(0x11):                 // ORA DP Indirect Indexed, Y
-        {
-            int opCycles = Binary::lower8BitsOf(mD) != 0 ? 1 : 0;
-            opCycles += opCodeAddressingCrossesPageBoundary(opCode) ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-            } else {
-                executeORA16Bit(opCode);
-                opCycles++;
-            }
-            addToProgramAddressAndCycles(2,5+opCycles);
-            break;
-        }
-        case(0x12):                 // ORA Direct page indirect
-        {
-            int opCycles = Binary::lower8BitsOf(mD) != 0 ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-            } else {
-                executeORA16Bit(opCode);
-                opCycles++;
-            }
-            addToProgramAddressAndCycles(2, 5+opCycles);
-            break;
-        }
-        case(0x13):                 // SR Indirect Indexed, Y
-        {
-            int opCycles = Binary::lower8BitsOf(mD) != 0 ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-                addToProgramAddressAndCycles(2,7);
-            } else {
-                executeORA16Bit(opCode);
-                addToProgramAddressAndCycles(2,8);
-            }
-            break;
-        }
-        case(0x15):                 // ORA DP Indexed, X
-        {
-            int opCycles = Binary::lower8BitsOf(mD) != 0 ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-                addToProgramAddressAndCycles(2,4+opCycles);
-            } else {
-                executeORA16Bit(opCode);
-                addToProgramAddressAndCycles(2,5+opCycles);
-            }
-            break;
-        }
-        case(0x17):                 // ORA DP Indirect Long Indexed, Y
-        {
-            int opCycles = Binary::lower8BitsOf(mD) != 0 ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-            } else {
-                executeORA16Bit(opCode);
-                opCycles++;
-            }
-            addToProgramAddressAndCycles(2,6+opCycles);
-            break;
-        }
-        case(0x19):                 // ORA Absolute Indexed, Y
-        {
-            int opCycles = opCodeAddressingCrossesPageBoundary(opCode) ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-                addToProgramAddressAndCycles(3,4+opCycles);
-            } else {
-                executeORA16Bit(opCode);
-                addToProgramAddressAndCycles(3,5+opCycles);
-            }
+            addToProgramAddressAndCycles(2, 6);
             break;
         }
         case(0x1D):                 // ORA Absolute Indexed, X
         {
-            int opCycles = opCodeAddressingCrossesPageBoundary(opCode) ? 1 : 0;
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-                addToProgramAddressAndCycles(3,4+opCycles);
-            } else {
-                executeORA16Bit(opCode);
-                addToProgramAddressAndCycles(3,5+opCycles);
+            if (opCodeAddressingCrossesPageBoundary(opCode)) {
+                addToCycles(1);
             }
+            addToProgramAddressAndCycles(3, 4);
             break;
         }
         case(0x1F):                 // ORA Absolute Long Indexed, X
         {
-            if (accumulatorIs8BitWide()) {
-                executeORA8Bit(opCode);
-                addToProgramAddressAndCycles(4,5);
-            } else {
-                executeORA16Bit(opCode);
-                addToProgramAddressAndCycles(4,6);
+            addToProgramAddressAndCycles(4, 5);
+            break;
+        }
+        case(0x19):                 // ORA Absolute Indexed, Y
+        {
+            if (opCodeAddressingCrossesPageBoundary(opCode)) {
+                addToCycles(1);
             }
+            addToProgramAddressAndCycles(3, 4);
+            break;
+        }
+        case(0x15):                 // ORA Direct Page Indexed, X
+        {
+            if (Binary::lower8BitsOf(mD) != 0) {
+                addToCycles(1);
+            }
+            addToProgramAddressAndCycles(2, 4);
+            break;
+        }
+        case (0x01):                // ORA Direct Page Indexed Indirect, X
+        {
+            if (Binary::lower8BitsOf(mD) != 0) {
+                addToCycles(1);
+            }
+            addToProgramAddressAndCycles(2, 6);
+            break;
+        }
+        case(0x11):                 // ORA Direct Page Indirect Indexed, Y
+        {
+            if (Binary::lower8BitsOf(mD) != 0) {
+                addToCycles(1);
+            }
+            if (opCodeAddressingCrossesPageBoundary(opCode)) {
+                addToCycles(1);
+            }
+            addToProgramAddressAndCycles(2, 5);
+            break;
+        }
+        case(0x17):                 // ORA Direct Page Indirect Long Indexed, Y
+        {
+            if (Binary::lower8BitsOf(mD) != 0) {
+                addToCycles(1);
+            }
+            addToProgramAddressAndCycles(2, 6);
+            break;
+        }
+        case(0x03):                // ORA Stack Relative
+        {
+            addToProgramAddressAndCycles(2, 4);
+            break;
+        }
+        case(0x13):                 // Stack Relative Indirect Indexed, Y
+        {
+            addToProgramAddressAndCycles(2, 7);
             break;
         }
         default:
